4a31c27972d31e4c2b3258182e2cc6111b08d235
Boone Ferry <boone.ferry@dexcarehealth.com>
2021-07-05 12:06:07 -0700

    fix: tests

--- deafwave/consensus/default_constants.py
+++ deafwave/consensus/default_constants.py
@@ -4,8 +4,7 @@ from .constants import ConsensusConstants
 
 testnet_kwargs = {
     "SLOT_BLOCKS_TARGET": 32,
-    # Must be less than half of SLOT_BLOCKS_TARGET
-    "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 16,
+    "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 16, # Must be less than half of SLOT_BLOCKS_TARGET
     "MAX_SUB_SLOT_BLOCKS": 128,  # Must be less than half of SUB_EPOCH_BLOCKS
     "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
     "SUB_SLOT_ITERS_STARTING": 2 ** 27,
@@ -13,30 +12,23 @@ testnet_kwargs = {
     # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
     "DIFFICULTY_CONSTANT_FACTOR": 2 ** 67,
     "DIFFICULTY_STARTING": 7,
-    # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
-    "DIFFICULTY_CHANGE_MAX_FACTOR": 3,
+    "DIFFICULTY_CHANGE_MAX_FACTOR": 3, # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
     # These 3 constants must be changed at the same time
     "SUB_EPOCH_BLOCKS": 384,  # The number of blocks per sub-epoch, mainnet 384
-    # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
-    "EPOCH_BLOCKS": 4608,
-    # The number of bits to look at in difficulty and min iters. The rest are zeroed
-    "SIGNIFICANT_BITS": 8,
-    # Max is 1024 (based on ClassGroupElement int size)
-    "DISCRIMINANT_SIZE_BITS": 1024,
-    # H(plot signature of the challenge) must start with these many zeroes
-    "NUMBER_ZERO_BITS_PLOT_FILTER": 9,
+    "EPOCH_BLOCKS": 4608, # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
+    "SIGNIFICANT_BITS": 8, # The number of bits to look at in difficulty and min iters. The rest are zeroed
+    "DISCRIMINANT_SIZE_BITS": 1024, # Max is 1024 (based on ClassGroupElement int size)
+    "NUMBER_ZERO_BITS_PLOT_FILTER": 9, # H(plot signature of the challenge) must start with these many zeroes
     "MIN_PLOT_SIZE": 32,  # 32 for mainnet
     "MAX_PLOT_SIZE": 50,
     "SUB_SLOT_TIME_TARGET": 600,  # The target number of seconds per slot, mainnet 600
     "NUM_SP_INTERVALS_EXTRA": 3,  # The number of sp intervals to add to the signage point
-    # The next block can have a timestamp of at most these many seconds in the future
-    "MAX_FUTURE_TIME": 5 * 60,
+    "MAX_FUTURE_TIME": 5 * 60, # The next block can have a timestamp of at most these many seconds in the future
     "NUMBER_OF_TIMESTAMPS": 11,  # Than the average of the last NUMBER_OF_TIMESTAMPS blocks
     # Used as the initial cc rc challenges, as well as first block back pointers, and first SES back pointer
     # We override this value based on the chain being run (testnet0, testnet1, mainnet, etc)
     # Default used for tests is std_hash(b'')
-    "GENESIS_CHALLENGE": bytes.fromhex("15c1626e087d479980667bbdde8464b8cf737066c20622e520feaa096e9f9a3e"),
-    # Forks of deafwave should change this value to provide replay attack protection. This is set to mainnet genesis chall
+    "GENESIS_CHALLENGE": bytes.fromhex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
     "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("299fc7442fd638bc739f7bdcff8ccad332e50f6f91556ad0c5267538f5421baa"),
     "GENESIS_POST_FARM_PUZZLE_HASH": bytes.fromhex(
         "95c259eaf17836095a7bfb5b1254b53c554985364c349f4a6764787c21d425ad"
@@ -64,8 +56,7 @@ testnet_kwargs = {
     "INITIAL_FREEZE_END_TIMESTAMP": 1620061200,  # Mon May 03 2021 17:00:00 GMT+0000
     "NETWORK_TYPE": 0,
     "MAX_GENERATOR_SIZE": 1000000,
-    # Number of references allowed in the block generator ref list
-    "MAX_GENERATOR_REF_LIST_SIZE": 512,
+    "MAX_GENERATOR_REF_LIST_SIZE": 512, # Number of references allowed in the block generator ref list
 }
 
 
