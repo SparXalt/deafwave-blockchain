Chia-Network/chia-blockchain

mozilla typo fixes (#5303)
* refactor is_fee_enough

* fix typo

Co-authored-by: Nikolaj Kuntner <>

chia/daemon/server.py
@@ -19,7 +19,7 @@ from websockets import ConnectionClosedOK, WebSocketException, WebSocketServerPr
 from chia.cmds.init_funcs import chia_init
 from chia.daemon.windows_signal import kill
 from chia.server.server import ssl_context_for_root, ssl_context_for_server
-from chia.ssl.create_ssl import get_mozzila_ca_crt
+from chia.ssl.create_ssl import get_mozilla_ca_crt
 from chia.util.chia_logging import initialize_logging
 from chia.util.config import load_config
 from chia.util.json_util import dict_to_json_str
@@ -51,8 +51,8 @@ service_plotter = "chia plots create"
 async def fetch(url: str):
     async with ClientSession() as session:
         try:
-            mozzila_root = get_mozzila_ca_crt()
-            ssl_context = ssl_context_for_root(mozzila_root)
+            mozilla_root = get_mozilla_ca_crt()
+            ssl_context = ssl_context_for_root(mozilla_root)
             response = await session.get(url, ssl=ssl_context)
             if not response.ok:
                 log.warning("Response not OK.")

chia/wallet/util/backup_utils.py
@@ -7,7 +7,7 @@ from blspy import AugSchemeMPL, PrivateKey, PublicKeyMPL, SignatureMPL
 from cryptography.fernet import Fernet
 
 from chia.server.server import ssl_context_for_root
-from chia.ssl.create_ssl import get_mozzila_ca_crt
+from chia.ssl.create_ssl import get_mozilla_ca_crt
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.hash import std_hash
 from chia.wallet.derive_keys import master_sk_to_backup_sk
@@ -72,8 +72,8 @@ def get_backup_info(file_path, private_key):
 
 
 async def post(session: aiohttp.ClientSession, url: str, data: Any):
-    mozzila_root = get_mozzila_ca_crt()
-    ssl_context = ssl_context_for_root(mozzila_root)
+    mozilla_root = get_mozilla_ca_crt()
+    ssl_context = ssl_context_for_root(mozilla_root)
     response = await session.post(url, json=data, ssl=ssl_context)
     return await response.json()
 

SparXalt/deafwave-blockchain

deafwave/daemon/server.py
@@ -19,7 +19,7 @@ from websockets import ConnectionClosedOK, WebSocketException, WebSocketServerPr
 from deafwave.cmds.init_funcs import deafwave_init
 from deafwave.daemon.windows_signal import kill
 from deafwave.server.server import ssl_context_for_root, ssl_context_for_server
-from deafwave.ssl.create_ssl import get_mozzila_ca_crt
+from deafwave.ssl.create_ssl import get_mozilla_ca_crt
 from deafwave.util.deafwave_logging import initialize_logging
 from deafwave.util.config import load_config, save_config
 from deafwave.util.json_util import dict_to_json_str
@@ -52,8 +52,8 @@ service_plotter = "deafwave plots create"
 async def fetch(url: str):
     async with ClientSession() as session:
         try:
-            mozzila_root = get_mozzila_ca_crt()
-            ssl_context = ssl_context_for_root(mozzila_root)
+            mozilla_root = get_mozilla_ca_crt()
+            ssl_context = ssl_context_for_root(mozilla_root)
             response = await session.get(url, ssl=ssl_context)
             if not response.ok:
                 log.warning("Response not OK.")

deafwave/wallet/util/backup_utils.py
@@ -7,7 +7,7 @@ from blspy import AugSchemeMPL, PrivateKey, PublicKeyMPL, SignatureMPL
 from cryptography.fernet import Fernet
 
 from deafwave.server.server import ssl_context_for_root
-from deafwave.ssl.create_ssl import get_mozzila_ca_crt
+from deafwave.ssl.create_ssl import get_mozilla_ca_crt
 from deafwave.util.byte_types import hexstr_to_bytes
 from deafwave.util.hash import std_hash
 from deafwave.wallet.derive_keys import master_sk_to_backup_sk
@@ -72,8 +72,8 @@ def get_backup_info(file_path, private_key):
 
 
 async def post(session: aiohttp.ClientSession, url: str, data: Any):
-    mozzila_root = get_mozzila_ca_crt()
-    ssl_context = ssl_context_for_root(mozzila_root)
+    mozilla_root = get_mozilla_ca_crt()
+    ssl_context = ssl_context_for_root(mozilla_root)
     response = await session.post(url, json=data, ssl=ssl_context)
     return await response.json()
 