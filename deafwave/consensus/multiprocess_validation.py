kevinforsterch/chia-blockchain

Validate summaries while syncing (#5284)
* block pre process check se matches wp

* test

* test

* test

* lint

* merge

* lint

chia/consensus/multiprocess_validation.py
@@ -17,6 +17,7 @@ from chia.consensus.pot_iterations import calculate_iterations_quality, is_overf
 from chia.full_node.mempool_check_conditions import get_name_puzzle_conditions
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.types.blockchain_format.sub_epoch_summary import SubEpochSummary
 from chia.types.full_block import FullBlock
 from chia.types.generator_types import BlockGenerator
 from chia.types.header_block import HeaderBlock
@@ -132,6 +133,7 @@ async def pre_validate_blocks_multiprocessing(
     npc_results: Dict[uint32, NPCResult],
     get_block_generator: Optional[Callable],
     batch_size: int,
+    wp_summaries: Optional[List[SubEpochSummary]] = None,
 ) -> Optional[List[PreValidationResult]]:
     """
     This method must be called under the blockchain lock
@@ -221,6 +223,13 @@ async def pre_validate_blocks_multiprocessing(
             block,
             None,
         )
+
+        if block_rec.sub_epoch_summary_included is not None and wp_summaries is not None:
+            idx = int(block.height / constants.SUB_EPOCH_BLOCKS) - 1
+            next_ses = wp_summaries[idx]
+            if not block_rec.sub_epoch_summary_included.get_hash() == next_ses.get_hash():
+                log.error("sub_epoch_summary does not match wp sub_epoch_summary list")
+                return None
         # Makes sure to not override the valid blocks already in block_records
         if not block_records.contains_block(block_rec.header_hash):
             block_records.add_block_record(block_rec)  # Temporarily add block to dict

tests/core/full_node/full_sync/test_full_sync.py
@@ -6,7 +6,9 @@ from typing import List
 
 import pytest
 
+from chia.full_node.weight_proof import _validate_sub_epoch_summaries
 from chia.protocols import full_node_protocol
+from chia.types.blockchain_format.sub_epoch_summary import SubEpochSummary
 from chia.types.full_block import FullBlock
 from chia.types.peer_info import PeerInfo
 from chia.util.hash import std_hash
@@ -343,3 +345,30 @@ class TestFullSync:
             await full_node_2.full_node.respond_block(full_node_protocol.RespondBlock(block))
 
         await time_out_assert(180, node_height_exactly, True, full_node_2, 999)
+
+    @pytest.mark.asyncio
+    async def test_block_ses_mismatch(self, three_nodes, default_1000_blocks):
+        full_node_1, full_node_2, full_node_3 = three_nodes
+        server_1 = full_node_1.full_node.server
+        server_2 = full_node_2.full_node.server
+        blocks = default_1000_blocks
+
+        for block in blocks[:501]:
+            await full_node_1.full_node.respond_block(full_node_protocol.RespondBlock(block))
+
+        peak1 = full_node_1.full_node.blockchain.get_peak()
+        full_node_2.full_node.sync_store.set_long_sync(True)
+        await server_2.start_client(PeerInfo(self_hostname, uint16(server_1._port)), full_node_2.full_node.on_connect)
+        wp = await full_node_1.full_node.weight_proof_handler.get_proof_of_weight(peak1.header_hash)
+        summaries1, _ = _validate_sub_epoch_summaries(full_node_1.full_node.weight_proof_handler.constants, wp)
+        summaries2 = summaries1
+        s = summaries1[1]
+        summaries2[1] = SubEpochSummary(
+            s.prev_subepoch_summary_hash,
+            s.reward_chain_hash,
+            s.num_blocks_overflow,
+            s.new_difficulty * 2,
+            s.new_sub_slot_iters * 2,
+        )
+        await full_node_2.full_node.sync_from_fork_point(0, 500, peak1.header_hash, summaries2)
+        await time_out_assert(180, node_height_exactly, True, full_node_2, 320)

SparXalt/deafwave-blockchain

deafwave/consensus/multiprocess_validation.py
@@ -17,6 +17,7 @@ from deafwave.consensus.pot_iterations import calculate_iterations_quality, is_o
 from deafwave.full_node.mempool_check_conditions import get_name_puzzle_conditions
 from deafwave.types.blockchain_format.coin import Coin
 from deafwave.types.blockchain_format.sized_bytes import bytes32
+from deafwave.types.blockchain_format.sub_epoch_summary import SubEpochSummary
 from deafwave.types.full_block import FullBlock
 from deafwave.types.generator_types import BlockGenerator
 from deafwave.types.header_block import HeaderBlock
@@ -143,6 +144,7 @@ async def pre_validate_blocks_multiprocessing(
     npc_results: Dict[uint32, NPCResult],
     get_block_generator: Optional[Callable],
     batch_size: int,
+    wp_summaries: Optional[List[SubEpochSummary]] = None,
 ) -> Optional[List[PreValidationResult]]:
     """
     This method must be called under the blockchain lock
@@ -235,6 +237,13 @@ async def pre_validate_blocks_multiprocessing(
             block,
             None,
         )
+
+        if block_rec.sub_epoch_summary_included is not None and wp_summaries is not None:
+            idx = int(block.height / constants.SUB_EPOCH_BLOCKS) - 1
+            next_ses = wp_summaries[idx]
+            if not block_rec.sub_epoch_summary_included.get_hash() == next_ses.get_hash():
+                log.error("sub_epoch_summary does not match wp sub_epoch_summary list")
+                return None
         # Makes sure to not override the valid blocks already in block_records
         if not block_records.contains_block(block_rec.header_hash):
             # Temporarily add block to dict

tests/core/full_node/full_sync/test_full_sync.py
@@ -6,7 +6,9 @@ from typing import List
 
 import pytest
 
+from deafwave.full_node.weight_proof import _validate_sub_epoch_summaries
 from deafwave.protocols import full_node_protocol
+from deafwave.types.blockchain_format.sub_epoch_summary import SubEpochSummary
 from deafwave.types.full_block import FullBlock
 from deafwave.types.peer_info import PeerInfo
 from deafwave.util.hash import std_hash
@@ -347,3 +349,30 @@ class TestFullSync:
             await full_node_2.full_node.respond_block(full_node_protocol.RespondBlock(block))
 
         await time_out_assert(180, node_height_exactly, True, full_node_2, 999)
+
+    @pytest.mark.asyncio
+    async def test_block_ses_mismatch(self, three_nodes, default_1000_blocks):
+        full_node_1, full_node_2, full_node_3 = three_nodes
+        server_1 = full_node_1.full_node.server
+        server_2 = full_node_2.full_node.server
+        blocks = default_1000_blocks
+
+        for block in blocks[:501]:
+            await full_node_1.full_node.respond_block(full_node_protocol.RespondBlock(block))
+
+        peak1 = full_node_1.full_node.blockchain.get_peak()
+        full_node_2.full_node.sync_store.set_long_sync(True)
+        await server_2.start_client(PeerInfo(self_hostname, uint16(server_1._port)), full_node_2.full_node.on_connect)
+        wp = await full_node_1.full_node.weight_proof_handler.get_proof_of_weight(peak1.header_hash)
+        summaries1, _ = _validate_sub_epoch_summaries(full_node_1.full_node.weight_proof_handler.constants, wp)
+        summaries2 = summaries1
+        s = summaries1[1]
+        summaries2[1] = SubEpochSummary(
+            s.prev_subepoch_summary_hash,
+            s.reward_chain_hash,
+            s.num_blocks_overflow,
+            s.new_difficulty * 2,
+            s.new_sub_slot_iters * 2,
+        )
+        await full_node_2.full_node.sync_from_fork_point(0, 500, peak1.header_hash, summaries2)
+        await time_out_assert(180, node_height_exactly, True, full_node_2, 320)