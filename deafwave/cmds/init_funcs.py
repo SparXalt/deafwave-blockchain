kevinforsterch/chia-blockchain

call core init if needed when `-c` is specified (#4823)
* call core init if needed when `-c` is specified

* check and propagate return codes

chia/cmds/init_funcs.py
@@ -243,8 +243,13 @@ def init(create_certs: Optional[Path], root_path: Path):
             else:
                 print(f"** Directory {create_certs} does not exist **")
         else:
-            print(f"** {root_path} does not exist **")
-            print("** Please run `chia init` to migrate or create new config files **")
+            print(f"** {root_path} does not exist. Executing core init **")
+            # sanity check here to prevent infinite recursion
+            if chia_init(root_path) == 0 and root_path.exists():
+                return init(create_certs, root_path)
+
+            print(f"** {root_path} was not created. Exiting **")
+            return -1
     else:
         return chia_init(root_path)
 

SparXalt/deafwave-blockchain

deafwave/cmds/init_funcs.py
@@ -253,9 +253,13 @@ def init(create_certs: Optional[Path], root_path: Path):
             else:
                 print(f"** Directory {create_certs} does not exist **")
         else:
-            print(f"** {root_path} does not exist **")
-            print(
-                "** Please run `deafwave init` to migrate or create new config files **")
+            print(f"** {root_path} does not exist. Executing core init **")
+            # sanity check here to prevent infinite recursion
+            if deafwave_init(root_path) == 0 and root_path.exists():
+                return init(create_certs, root_path)
+
+            print(f"** {root_path} was not created. Exiting **")
+            return -1
     else:
         return deafwave_init(root_path)
 