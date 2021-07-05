kevinforsterch/chia-blockchain

Less log getter setup calls (#6123)
* init

* remove whitespace

* docs

* rem double colon

* more type annotation

* retrigger timed out test

Co-authored-by: wjblanke

chia/cmds/units.py
@@ -1,9 +1,9 @@
 from typing import Dict
 
-# The rest of the codebase uses mojos everywhere. Only uses these units
-# for user facing interfaces
+# The rest of the codebase uses mojos everywhere.
+# Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "chia": 10 ** 12,  # 1 chia (XCH) is 1,000,000,000,000 mojo (1 Trillion)
+    "chia": 10 ** 12,  # 1 chia (XCH) is 1,000,000,000,000 mojo (1 trillion)
     "mojo:": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
 }

SparXalt/deafwave-blockchain

deafwave/cmds/units.py
@@ -1,10 +1,9 @@
 from typing import Dict
 
-# The rest of the codebase uses mojos everywhere. Only uses these units
-# for user facing interfaces
+# The rest of the codebase uses mojos everywhere.
+# Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    # 1 deafwave (ZZZ) is 1,000,000,000,000 mojo (1 trillion)
-    "deafwave": 10 ** 12,
+    "deafwave": 10 ** 12,  # 1 deafwave (ZZZ) is 1,000,000,000,000 mojo (1 trillion)
     "mojo:": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
 }