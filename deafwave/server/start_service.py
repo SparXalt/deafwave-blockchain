kevinforsterch/chia-blockchain

service singleton (#6735)

chia/server/start_service.py
@@ -6,6 +6,7 @@ import signal
 from sys import platform
 from typing import Any, Callable, List, Optional, Tuple
 
+from chia.daemon.server import singleton, service_launch_lock_path
 from chia.server.ssl_context import chia_ssl_ca_paths, private_ssl_ca_paths
 
 try:
@@ -163,6 +164,10 @@ class Service:
             )
 
     async def run(self) -> None:
+        lockfile = singleton(service_launch_lock_path(self.root_path, self._service_name))
+        if lockfile is None:
+            self._log.error(f"{self._service_name}: already running")
+            raise ValueError(f"{self._service_name}: already running")
         await self.start()
         await self.wait_closed()
 

SparXalt/deafwave-blockchain

deafwave/server/start_service.py
@@ -6,6 +6,7 @@ import signal
 from sys import platform
 from typing import Any, Callable, List, Optional, Tuple
 
+from deafwave.daemon.server import singleton, service_launch_lock_path
 from deafwave.server.ssl_context import deafwave_ssl_ca_paths, private_ssl_ca_paths
 
 try:
@@ -168,6 +169,10 @@ class Service:
             )
 
     async def run(self) -> None:
+        lockfile = singleton(service_launch_lock_path(self.root_path, self._service_name))
+        if lockfile is None:
+            self._log.error(f"{self._service_name}: already running")
+            raise ValueError(f"{self._service_name}: already running")
         await self.start()
         await self.wait_closed()
 